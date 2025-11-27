ddd58291a7d09eb8d6b0100ed37d97c6d6a9b65d
angel.dhakal <angeldhakal97@gmail.com>
2025-11-27 01:53:57 -0500

    feat: Implement Rofi-based wallpaper selector and gamemode toggle with dedicated themes, update Hyprland and Waybar styling, and remove old wallpaper script.

--- .config/rofi/gamemode.sh
+++ .config/rofi/gamemode.sh
@@ -6,10 +6,11 @@
 LOCK_FILE="$HOME/.cache/gamemode.active"
 
 if [ -z "$@" ]; then
+    ICON="$HOME/dotfiles/.config/rofi/icons/game.png"
     if [ -f "$LOCK_FILE" ]; then
-        echo "Disable Game Mode"
+        echo -en "Disable Game Mode\0icon\x1f$ICON\n"
     else
-        echo "Enable Game Mode"
+        echo -en "Enable Game Mode\0icon\x1f$ICON\n"
     fi
 else
     if [ "$@" = "Enable Game Mode" ]; then
@@ -19,11 +20,11 @@ else
         # Disable animations and decorations
         hyprctl --batch "\
             keyword animations:enabled 0;\
-            keyword decoration:drop_shadow 0;\
+            keyword decoration:shadow:enabled 0;\
             keyword decoration:blur:enabled 0;\
             keyword general:gaps_in 0;\
             keyword general:gaps_out 0;\
-            keyword decoration:rounding 0"
+            keyword decoration:rounding 0" > /dev/null
             
         notify-send -u low -t 2000 "Game Mode" "Enabled: Animations & Effects Disabled"
         
@@ -32,7 +33,7 @@ else
         rm -f "$LOCK_FILE"
         
         # Reload Hyprland config to restore defaults
-        hyprctl reload
+        hyprctl reload > /dev/null
         
         notify-send -u low -t 2000 "Game Mode" "Disabled: Normal Mode Restored"
     fi
