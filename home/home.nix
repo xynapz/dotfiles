b80425beaf4aeb07ec312a20f2817c8d70bb9054
xynapz <xynapz@aol.com>
2026-01-30 14:32:24 -0500

    chore: Remove fzf, zoxide, and delta from home packages.

--- home/home.nix
+++ home/home.nix
@@ -45,7 +45,7 @@
   home.packages = with pkgs; [
     wezterm foot firefox thunar
     nodejs python3 go rustup gcc gnumake cmake
-    ripgrep fd fzf bat eza zoxide delta dust duf procs bottom
+    ripgrep fd bat eza dust duf procs bottom
     imv mpv wl-clipboard cliphist grim slurp swappy wf-recorder
     pavucontrol blueman libnotify unzip p7zip tree calc
     iosevka jetbrains-mono sourceHighlight imagemagick
