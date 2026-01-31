eacb74390e27ac1d1d8bd8750b9deb35ba29af4a
xynapz <xynapz@aol.com>
2026-01-31 15:56:40 -0500

    feat: Implement modular Eww configuration, introducing a new bar, control panel, and calendar widgets.

--- home/modules/eww/default.nix
+++ home/modules/eww/default.nix
@@ -1,8 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ eww ];
+  home.packages = with pkgs; [ 
+    eww
+    jaq 
+    socat 
+    pamixer 
+    brightnessctl 
+    playerctl 
+  ];
 
   xdg.configFile."eww/eww.yuck".source = ./eww.yuck;
   xdg.configFile."eww/eww.scss".source = ./eww.scss;
+  xdg.configFile."eww/yuck".source = ./yuck;
+  xdg.configFile."eww/scss".source = ./scss;
+  xdg.configFile."eww/scripts".source = ./scripts;
 }
