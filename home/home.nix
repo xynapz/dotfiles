8cf80e6a19c883a0e5683bcc61f18593d941718d
xynapz <xynapz@aol.com>
2026-01-31 18:34:26 -0500

    fix: bar module moved to waybar.nix

--- home/home.nix
+++ home/home.nix
@@ -6,7 +6,7 @@
     ./modules/shell.nix
     ./modules/wezterm.nix
     ./modules/sway.nix
-    ./modules/bar.nix
+    ./modules/waybar.nix
     ./modules/launcher.nix
     ./modules/notifications.nix
     ./modules/screenshots.nix
