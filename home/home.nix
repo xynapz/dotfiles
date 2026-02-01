0e40ef682ebcfa2341dc6336bafe51c8f4134692
xynapz <xynapz@aol.com>
2026-02-01 01:53:59 -0500

    package: poppler_utils, specificaly pdftocairo req

--- home/home.nix
+++ home/home.nix
@@ -50,7 +50,7 @@
     imv mpv wl-clipboard cliphist grim slurp swappy wf-recorder
     pavucontrol libnotify unzip p7zip tree calc
     iosevka jetbrains-mono sourceHighlight imagemagick
-    xarchiver nixd antigravity
+    xarchiver nixd antigravity poppler_utils
   ];
 
   # XDG
