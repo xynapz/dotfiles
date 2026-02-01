87701e4fc99b7c2bb190c838680dedf9281eed46
xynapz <xynapz@aol.com>
2026-01-31 20:42:10 -0500

    refactor: Move Fuzzel configuration from `launcher.nix` to a dedicated `fuzzel.ini` file.

--- home/modules/launcher.nix
+++ home/modules/launcher.nix
@@ -4,10 +4,6 @@
 {
   programs.fuzzel = {
     enable = true;
-    settings = {
-      main = { font = "IBM Plex Mono:size=14"; width = 50; lines = 12; prompt = "❯ "; terminal = "wezterm"; icons-enabled = "yes"; };
-      colors = { background = "2e3440ee"; text = "eceff4ff"; prompt = "88c0d0ff"; match = "88c0d0ff"; selection = "3b4252ff"; selection-text = "eceff4ff"; border = "4c566aff"; };
-      border = { width = 2; radius = 8; };
-    };
   };
+  xdg.configFile."fuzzel/fuzzel.ini".source = ../../config/fuzzel/fuzzel.ini;
 }
