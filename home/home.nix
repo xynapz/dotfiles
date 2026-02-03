b38eadec6448b6aab274ba3576d7b6650ff3b9b3
xynapz <xynapz@aol.com>
2026-02-02 20:47:46 -0500

    new packages

--- home/home.nix
+++ home/home.nix
@@ -52,7 +52,7 @@
     pavucontrol libnotify unzip p7zip tree calc
     iosevka jetbrains-mono sourceHighlight imagemagick
     xarchiver nixd antigravity poppler-utils
-    kdePackages.kate nautilus discord
+    kdePackages.kate nautilus discord gnome-calculator gnome-calendar
   ];
 
   # XDG
